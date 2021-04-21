bae0b4984521a519e668d262d961f271cae352b2
Karol Cobos <Karol Cobos@LAPTOP-6T4N4FV2>
2021-04-20 23:17:43 -0500

    Ejercicios dados en clases

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author jorge Cobos
+ *
+ */
+public class Ejercicio1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+    int n1;
+    int n2;
+     System.out.println("-----------------Ejercicio 1 -------------");
+     System.out.println("Introduzca el primer numero");
+     n1 = sc.nextInt();
+     System.out.println("Introduzca el segundo numero");
+     n2 = sc.nextInt();
+       if(n1>n2){
+      System.out.println("el primer numero es mayor");
+       }else{   
+       System.out.println("el segundo numero es mayor");
+      //System.out.println("-----------------Ejercicio 2 -------------");
+     // System.out.println("Introduzca el primer numero");
+     //  n1 = sc.nextInt();
+     //System.out.println("Introduzca el segundo numero");
+      // n2 = sc.nextInt();
+     //     if(n1>n2){
+    //  System.out.println("El primer numero: "+n1+" es el mayor y el segundo numero: "
+          //    + " "+n2+ " es el menor");
+      // }else{   
+    // System.out.println("El segundo numero: "+n2+" es el mayor y el primer numero: "
+       //      + " "+n1+  " es el menor");
+    // System.out.println("-----------------Ejercicio 3 -------------");
+     //System.out.println("Introduzca el primer numero");
+     // n1 = sc.nextInt();
+   //  System.out.println("Introduzca el segundo numero");
+     //  n2 = sc.nextInt();
+        //  if(n1>n2){
+        //  System.out.println("El primer numero: "+n1+" es el mayor y el segundo: "
+          //        + " numero "+n2+ " es el menor");
+        //  } else if (n2>n1){
+          //    System.out.println("El segundo numero: "+n2+" es el mayor y el "
+           //           + "primer numero: "+n1+  " es el menor");
+        //  } else{
+     // System.out.println("Los dos numeros introduccidos son iguales");
+   //  System.out.println("-----------------Ejercicio 4 -------------");
+         //  System.out.println("Año actual : 2021");
+         //  System.out.println("Introduzca el año");
+         //    n1 = sc.nextInt();
+         //  if(n1<2021){
+         //  n2=2021-n1;
+         //  System.out.println("han faltan "+n2+" años");
+         //   }else{
+          //   n2=n1-2021;
+          //  System.out.println("han pasado "+n2+" años");
+        //  System.out.println("-----------------Ejercicio 5 -------------");
+         // System.out.println("Introduzca el primer numero");
+         // n1 = sc.nextInt();
+         // if(n1>=0){
+          //    System.out.println("El numero es positivo");
+         //  }else{
+          //    System.out.println("El numero es negativo");
+       //   System.out.println("-----------------Ejercicio 6 -------------");
+         // System.out.println("Introduzca el primer numero");
+         // n1 = sc.nextInt();
+          //if(n1>=10 && n1<=99){
+          //     System.out.println("tiene 2 cifras");
+          //}else{
+           //   System.out.println("Distinto de 2 cifras");
+          }
+                } 
+    }
+    
+    
+
